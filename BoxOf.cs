35102ee4835687854a0940c2fdb894178864c992
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-05-07 08:50:28 +0300

    add Box.cs, BoxOf.cs

---
+++ BoxOf.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOf
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T inputValue)
+        {
+            this.Value = inputValue;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Value.GetType()}: {this.Value}";
+        }
+    }
+}
